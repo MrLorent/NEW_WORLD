f55642541a6a532107cfe38be003a7dd179f684e
TLORENT <lorent.tanguy@gmail.com>
2022-12-21 00:10:46 +0100

    ✨ Creation of WeepingWillowBase

---
+++ Assets/Scripts/LSystem/Bases/WeepingWillowBase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "L-System/Base/Weeping Willow")]
+public class WeepingWillowBase : LSystemBase
+{
+    WeepingWillowBase()
+    {
+        /*====== AXIOM ======*/
+        axiom = "F(l)+(45)A";
+
+        /*=== AXIOM PARAMS ===*/
+        start_width = 1;
+        start_length = 10;
+
+        /*====== RULES ======*/
+        rules = new Dictionary<char, string>()
+        {
+            {'A', "F(l)F(l)[&(a)!(vr)\"(lr)A]+(d1)[&(a)!(vr)\"(lr)A]+(d2)[&(a)!(vr)\"(lr)A]"},
+        };
+
+        /*==== CONSTANTS ====*/
+        constants = new Dictionary<string, float>()
+        {
+            {"d1",  94.74F  },
+            {"d2",  132.63F },
+            {"a",   18.85F  },
+            {"lr",  0.902F  },
+            {"vr",  0.577F  },
+            {"e",   0.22F   },
+        };
+    }
+}
